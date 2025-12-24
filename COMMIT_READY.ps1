3aeea2ae3ecbe7d9bf3970495af69884f1e9fc05
Vasilis <30999851+bs1gr@users.noreply.github.com>
2025-12-25 00:41:47 +0200

    CI update and documentation for v1.12.7

--- COMMIT_READY.ps1
+++ COMMIT_READY.ps1
@@ -67,7 +67,7 @@
     # Fix formatting and import issues automatically
 
 .NOTES
-Version: 1.12.6
+Version: 1.12.7
     Created: 2025-11-27
     Consolidates: COMMIT_PREP, PRE_COMMIT_CHECK, PRE_COMMIT_HOOK, SMOKE_TEST_AND_COMMIT_PREP
 
